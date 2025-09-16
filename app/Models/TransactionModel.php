0b6fdb80af150226a43e488b92b3762b17158a3e
gaprass7 <gnngzr@gmail.com>
2025-09-17 02:48:54 +0700

    model

---
+++ app/Models/TransactionModel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class TransactionModel extends Model
+{
+    protected $table      = 'transactions';
+    protected $primaryKey = 'id';
+
+    protected $allowedFields = ['user_id', 'total'];
+    protected $useTimestamps = true;
+}
